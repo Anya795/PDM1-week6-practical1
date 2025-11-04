63b56fc54d3200e793f7bc4ead2426e2d135c092
Anya795 <psd540@york.ac.uk>
2025-11-04 17:30:40 +0000

    text

---
+++ exercise1-1/sketch.js
@@ -0,0 +1,11 @@
+function setup(){
+    createCanvas(600,600);
+}
+
+function draw(){
+    background(255);
+    textSize(35);
+    textAlign(RIGHT, BOTTOM);
+    fill(0,200, 100);
+    text("Lorem ipsum dolor sit amet", 300, 300, 300, 300);
+}
---
+++ exercise1-2/sketch.js
@@ -0,0 +1,9 @@
+function setup(){
+    createCanvas(600,600); 
+}
+
+function draw(){
+    background(0);
+    fill(255,0,0);
+    
+}
