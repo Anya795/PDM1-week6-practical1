8c03ae621ed34a47357edb99d83432dca0dd64ea
Anya795 <psd540@york.ac.uk>
2025-11-13 11:00:55 +0000

    ccc

--- exercise1-3/sketch.js
+++ exercise1-3/sketch.js
@@ -1,18 +1,28 @@
 let myFont;
+const browserType = "Anya Soni";
+browserType.length;
 
 function setup(){
     createCanvas(600,600);
+    frameRate(30);
 }
 
 function draw(){
     background(0);
-    textAlign(CENTER, CENTER);
-    fill(255,0,0);
-    textSize(90);
-    textFont(myFont);
-    text("Anya Soni.", 300, 300,)
+    drawCursor();
+    let fps = frameRate();
+  text(fps, 50, 50);
 }
 
 function preload(){
     myFont = loadFont("assets/tomatoes-O8L8.ttf");
 }
+
+function drawCursor(){
+    textAlign(CENTER, CENTER);
+    fill(255,0,0);
+    textSize(70);
+    textFont(myFont);
+    text("Anya Soni.", 300, 300,)
+}
+
